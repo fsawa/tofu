b1f5750e9f40d133a68c1e524d5b8f3b525b6771
FUJISAWA-8\yasuhito <dinks223@gmail.com>
2017-07-04 01:46:14 +0900

    math 円周率の定数値定義

--- Source/tofu/math/MathCommon.hpp
+++ Source/tofu/math/MathCommon.hpp
@@ -17,6 +17,29 @@
 namespace tofu {
 namespace math {
 
+//------------------------------------------------------------------------------
+// 定数
+
+/// 定数群
+template <typename T>
+struct Konst
+{
+	/// 円周率
+	static constexpr T pi = static_cast<T>(3.1415926535897932384626433832795);
+	
+	/// 円周率 x2
+	static constexpr T pi2 = static_cast<T>(3.1415926535897932384626433832795 * 2);
+	
+	/// 円周率 / 2
+	static constexpr T pi_div2 = static_cast<T>(3.1415926535897932384626433832795 / 2);
+};
+
+/// 円周率
+template <typename T>
+constexpr T Pi() { return Konst<T>::pi; }
+
+//------------------------------------------------------------------------------
+
 /// 絶対値
 template <typename T>
 constexpr T Abs( T x ) noexcept
