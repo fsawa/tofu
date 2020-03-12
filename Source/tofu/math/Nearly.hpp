ea2ecf210ed01829dc1a3c6134119fc35dcdf86e
FUJISAWA-8\yasuhito <dinks223@gmail.com>
2020-03-13 01:59:38 +0900

    誤差許容判定クラス実装

---
+++ Source/tofu/math/Nearly.hpp
@@ -0,0 +1,132 @@
+﻿//------------------------------------------------------------------------------
+/**
+ * @file    Nearly.hpp
+ * @brief   許容誤差判定
+ * @author  y.fujisawa
+ * @par     copyright
+ * Copyright (C) 2017 Yasuhito Fujisawa\n
+ * This software is released under the MIT License, see LICENSE
+*/
+//------------------------------------------------------------------------------
+#pragma once
+
+#include <tofu.hpp>
+
+namespace tofu {
+namespace math {
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief      
+/// 
+////////////////////////////////////////////////////////////////////////////////////////////////
+template <typename T>
+class NearlyChecker
+{
+	using self_type = NearlyChecker;
+	
+//**************************************************************
+//              : public
+//**************************************************************
+public:
+	
+// DEFINE
+	
+	using value_type = T;
+	
+	static constexpr T kDefaultTorerance = static_cast<T>(0.0001); ///< デフォルト許容誤差
+	
+// FUNCTION
+	
+	/// constructor
+	NearlyChecker() noexcept
+		: m_value( static_cast<T>(0.0) )
+		, m_torerance( kDefaultTorerance )
+	{
+	}
+	
+	explicit NearlyChecker( T torerance ) noexcept
+		: m_value( static_cast<T>(0.0) )
+		, m_torerance(torerance)
+	{
+	}
+	
+	/// 値の設定
+	self_type&  set( T value )
+	{
+		m_value = value;
+		return *this;
+	}
+	
+	/// 値を設定した別オブジェクトを作成する
+	self_type  make( T value ) const
+	{
+		return NearlyChecker(m_torerance).set(value);
+	}
+	
+	/// 許容最小値取得
+	T  min() const { return m_value - m_torerance; }
+	
+	/// 許容最大値取得
+	T  max() const { return m_value + m_torerance; }
+	
+//**************************************************************
+//              : private
+//**************************************************************
+private:
+	
+// DEFINE
+	
+// FUNCTION
+	
+// VARIABLE
+	
+	value_type m_value;
+	value_type m_torerance;
+};
+// << NearlyChecker
+
+/// 
+template <typename T>
+bool operator==( T value, NearlyChecker<T> n )
+{
+	return n.min() <= value && value <= n.max();
+}
+
+/// 
+template <typename T>
+bool operator==( NearlyChecker<T> n, T value )
+{
+	return n.min() <= value && value <= n.max();
+}
+
+/// 
+template <typename T>
+bool operator!=( T value, NearlyChecker<T> n )
+{
+	return !(value == n);
+}
+
+/// 
+template <typename T>
+bool operator!=( NearlyChecker<T> n, T value )
+{
+	return !(n == value);
+}
+
+
+/// 誤差許容判定オブジェクト作成
+template <typename T>
+NearlyChecker<T> Nearly( T value )
+{
+	return NearlyChecker<T>().set(value);
+}
+
+/// 誤差許容判定オブジェクト作成
+template <typename T>
+NearlyChecker<T> Nearly( T value, T torerance )
+{
+	return NearlyChecker<T>( torerance ).set(value);
+}
+
+} // math
+} // tofu
---
+++ Test/math/Nearly_test.cpp
@@ -0,0 +1,64 @@
+﻿//------------------------------------------------------------------------------
+/**
+ * @file    Nearly_test.cpp
+ * @brief   ほぼ等しい判定のテスト
+ * @author  y.fujisawa
+ * @par     copyright
+ * Copyright (C) 2017 Yasuhito Fujisawa\n
+ * This software is released under the MIT License, see LICENSE
+*/
+//------------------------------------------------------------------------------
+#include <tofu/math/Nearly.hpp>
+
+//#include <cfloat>
+#include <iostream>
+#include <iomanip>
+#include <iutest.hpp>
+
+IUTEST(math, Nearly)
+{
+	float a = 0.f;
+	float b = 0.f;
+	
+	for(int i=0; i<10; ++i){
+		a += 0.1f;
+	}
+	for(int i=0; i<1000; ++i){
+		b += 0.1f;
+	}
+	
+	std::cout << std::fixed;
+	
+	std::cout << std::setprecision(std::numeric_limits<float >::max_digits10)
+		<< FLT_EPSILON << " // epsilon" << std::endl;
+	
+	std::cout << std::setprecision(std::numeric_limits<float >::max_digits10)
+		<< FLT_EPSILON*100 << " // epsilon*100" << std::endl;
+	
+	std::cout << std::setprecision(std::numeric_limits<float >::max_digits10)
+		<< (a - 1.f) << " ( " << a << std::endl;
+	
+	std::cout << std::setprecision(std::numeric_limits<float >::max_digits10)
+		<< (b - 100.f) << " ( " << b << std::endl;
+
+	// 誤差が出ることを確認
+	IUTEST_ASSERT_NE( a, 1.f );
+	IUTEST_ASSERT_NE( b, 100.f );
+
+	// Nearlyの出番
+	
+	IUTEST_ASSERT_NE( 1.f, a ); // 1.fと比較NG
+	IUTEST_ASSERT_NE( tofu::math::Nearly(1.f, 0.f), a ); // 許容誤差が小さすぎてもNG
+	IUTEST_ASSERT_EQ( tofu::math::Nearly(1.f), a );
+	//IUTEST_ASSERT_EQ( tofu::math::Nearly(1.f).tolerance(0.001f), a );
+	//IUTEST_ASSERT_EQ( tofu::math::Nearly(1.f).epsilonRate(100.f), a );
+
+	// オブジェクト化して利用
+	{
+		auto nearly = tofu::math::NearlyChecker<float>(0.0001f);
+		
+		IUTEST_ASSERT_EQ( nearly.set(1.f), a );
+		IUTEST_ASSERT_EQ( nearly.make(1.f), a );
+	}
+
+}
