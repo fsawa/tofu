0af7b6c35bcc6a0cb233fc590587b8dbb10b922f
FUJISAWA-8\yasuhito <dinks223@gmail.com>
2020-10-18 02:42:29 +0900

    Finalizer作成

--- Source/tofu/util/Finalizer.hpp
+++ Source/tofu/util/Finalizer.hpp
@@ -1,7 +1,7 @@
 ﻿//------------------------------------------------------------------------------
 /**
  * @file    Finalizer.hpp
- * @brief   
+ * @brief   インスタンスの開放や終了処理を一括で実行する為のクラス
  * @author  y.fujisawa
  * @par     copyright
  * Copyright (C) 2020 Yasuhito Fujisawa\n
@@ -11,8 +11,123 @@
 #pragma once
 
 #include <tofu.hpp>
+#include <tofu/util/DisallowCopy.hpp>
+
+#include <list>
+#include <functional>
 
 namespace tofu {
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+/// @brief      インスタンスの開放や終了処理を一括で実行する為のクラス
+/// 
+/// @note
+/// 登録と逆順で処理を行う。
+////////////////////////////////////////////////////////////////////////////////////////////////
+class Finalizer
+{
+	typedef Finalizer  self_type;
+	
+	// コピー禁止
+	TOFU_DISALLOW_COPY(Finalizer);
+	
+//**************************************************************
+//              : public
+//**************************************************************
+public:
+	
+// DEFINE
+	
+// FUNCTION
+	
+	/// constructor
+	Finalizer() {}
+	
+	/// destructor
+	/*virtual*/ ~Finalizer()
+		{
+			finalize();
+		}
+	
+	//==============================
+	
+	/// deleteするインスタンスの登録
+	template <typename T>
+	T*  add( T* ptr )
+		{
+			addFunctor( Deleter<T>(ptr) );
+			return ptr;
+		}
+	
+	/// delete[]するインスタンスの登録
+	template <typename T>
+	T*  addArray( T* ptr )
+		{
+			addFunctor( ArrayDeleter<T>(ptr) );
+			return ptr;
+		}
+	
+	/// 実行する関数オブジェクトの登録
+	template <typename Functor>
+	void  addFunctor( const Functor& f )
+		{
+			// 登録と逆順で処理しやすいように前に追加
+			m_funcList.push_front(f);
+		}
+	
+	/// 終了処理を実行
+	void  finalize()
+	{
+		for (auto& func : m_funcList)
+		{
+			func();
+		}
+		m_funcList.clear();
+	}
+	
+//**************************************************************
+//              : private
+//**************************************************************
+private:
+	
+// DEFINE
+	
+	template <typename T>
+	class Deleter
+	{
+	public:
+		explicit Deleter(T* ptr) : m_ptr(ptr) {}
+		
+		void operator()()
+			{
+				delete m_ptr;
+				m_ptr = nullptr;
+			}
+	private:
+		T* m_ptr;
+	};
+	
+	template <typename T>
+	class ArrayDeleter
+	{
+	public:
+		explicit ArrayDeleter(T* ptr) : m_ptr(ptr) {}
+		
+		void operator()()
+			{
+				delete[] m_ptr;
+				m_ptr = nullptr;
+			}
+	private:
+		T* m_ptr;
+	};
+	
+// FUNCTION
+	
+// VARIABLE
+	
+	std::list< std::function<void ()> >  m_funcList;
+};
+// << Finalizer
 
 } // tofu
---
+++ Test/Finalizer_test.cpp
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+/**
+ * @file    Finalizer_test.cpp
+ * @brief   Finalizerテスト
+ * @author  y.fujisawa
+ * @par     copyright
+ * Copyright (C) 2020 Yasuhito Fujisawa\n
+ * This software is released under the MIT License, see LICENSE
+*/
+//------------------------------------------------------------------------------
+#include <tofu/util/Finalizer.hpp>
+#include <tofu/util/LooseSingleton.hpp>
+
+#include <iostream>
+#include <iutest.hpp>
+
+namespace
+{
+	int g_add_count = 0;
+	int g_fin_count = 0;
+	bool g_bad_order = false;
+	
+	class A
+	{
+	public:
+		A()  {}
+		~A() { std::cout << "~A" << std::endl;}
+	};
+
+	class SingleA : public tofu::LooseSingleton<SingleA>
+	{
+		int m_count;
+	public:
+		SingleA() : m_count(g_add_count++) {}
+		~SingleA() { std::cout << "~SingleA" << std::endl; if(m_count != --g_fin_count){g_bad_order = true;} }
+		void func() { std::cout << "SingleA::func" << std::endl; }
+	};
+	class SingleB : public tofu::LooseSingleton<SingleB>
+	{
+		int m_count;
+	public:
+		SingleB() : m_count(g_add_count++) {}
+		~SingleB() { std::cout << "~SingleB" << std::endl; if(m_count != --g_fin_count){g_bad_order = true;} }
+		void func() { std::cout << "SingleB::func" << std::endl; }
+	};
+
+	static void func1()
+	{
+		std::cout << "func1" << std::endl;
+	}
+}
+
+IUTEST(util, Finalizer)
+{
+	{
+		tofu::Finalizer fin;
+		
+		fin.add( new SingleA )->func();
+		fin.addFunctor(func1);
+		fin.addArray( new A[3] );
+		fin.add( new SingleB )->func();
+		
+		g_fin_count = g_add_count;
+		fin.finalize();
+
+		IUTEST_ASSERT((g_bad_order == false));
+		
+		IUTEST_ASSERT_EQ( SingleA::ExistInstance(), false);
+		IUTEST_ASSERT_EQ( SingleB::ExistInstance(), false);
+	}
+}
