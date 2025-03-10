2f2f47abee38ad80ba1c30342850e829d42cfc97
YASU-NOTE\dinks <dinks223@gmail.com>
2025-03-11 00:27:14 +0900

    [Tools] v143でのテスト

--- Tools/RunTest.bat
+++ Tools/RunTest.bat
@@ -5,7 +5,9 @@ pushd %~dp0
 rem ..\Build\vs2017\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=math.*
 rem ..\Build\vs2017\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=util.*
 rem ..\Build\vs2019\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=util.*
-..\Build\vs2022\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=util.*
+rem ..\Build\vs2022\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=util.*
+rem ..\Build\vs2022\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=*
+..\Build\vs2022\tofu_test\x64\Debug-v143\tofu_test.exe --iutest_filter=*
 
 :END
 
