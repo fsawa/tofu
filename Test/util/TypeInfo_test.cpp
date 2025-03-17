80320a8c3e1e5b722a429515f3d6eae9b5d486f0
YASU-NOTE\dinks <dinks223@gmail.com>
2025-03-18 02:01:42 +0900

    [test] TypeInfo 形名関連置き換え

--- Test/util/TypeInfo_test.cpp
+++ Test/util/TypeInfo_test.cpp
@@ -17,16 +17,54 @@ namespace test
 {
 	struct A {};
 	class B; // 宣言のみ
+
+	enum E1{};
+	enum class E2{};
 }
 
 // 無名名前空間
 namespace
 {
 	class C;
+
+	template <typename T>
+	void PrintDebugTypeName()
+	{
+		std::cout << tofu::TypeName<T>::TestValue.view() << std::endl;
+		std::cout << "    " << tofu::TypeName<T>::Value.view() << std::endl;
+	}
 }
 
 IUTEST(util, TypeInfo)
 {
+	std::cout << "-- Print --" << std::endl;
+	PrintDebugTypeName<int>();
+	PrintDebugTypeName<int&>();
+	PrintDebugTypeName<int*>();
+	PrintDebugTypeName<int**>();
+	PrintDebugTypeName<int[]>();
+	PrintDebugTypeName<int[3]>();
+	PrintDebugTypeName<const int[3][4]>();
+	PrintDebugTypeName<const volatile int&>();
+	PrintDebugTypeName<test::A>();
+	PrintDebugTypeName<const volatile test::A* const>();
+	PrintDebugTypeName<C>();
+	PrintDebugTypeName<test::E1>();
+	PrintDebugTypeName<test::E2>();
+	PrintDebugTypeName<void(int)>();
+	std::cout << "----" << std::endl;
+
+	// mpl::StringData
+	static_assert(tofu::mpl::String<"hoge">::Length == 4);
+	static_assert(tofu::mpl::StringData("hoge").Length == 4);
+	static_assert((tofu::mpl::StringData("123") + tofu::mpl::StringData("4567")).Length == 7);
+	static_assert((tofu::mpl::StringData("123") + "4567").Length == 7);
+	static_assert(("123" + tofu::mpl::StringData("4567")).Length == 7);
+
+	//static_assert(tofu::TypeInfoOf<test::A>::TypeName.size() == 7);
+	//IUTEST_ASSERT_EQ( tofu::TypeInfoOf<test::A>::TypeName(), std::string_view("test::A") );
+	//IUTEST_ASSERT_EQ( tofu::TypeInfoOf<C>::TypeName(), std::string_view("::C") );
+
 	IUTEST_ASSERT(!tofu::GetTypeInfo<int>().isConst());
 	//IUTEST_ASSERT(!tofu::GetTypeInfo<int>().isVolatile());
 
@@ -57,35 +95,53 @@ IUTEST(util, TypeInfo)
 
 	{
 		auto& info = tofu::GetTypeInfo<int>();
-		std::cout << info.getName() << std::endl;
+		std::cout << "-- int -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 	{
 		auto& info = tofu::GetTypeInfo<const int>();
-		std::cout << info.getName() << std::endl;
+		std::cout << "-- const int -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 	{
 		auto& info = tofu::GetTypeInfo<void>();
-		std::cout << info.getName() << std::endl;
+		std::cout << "-- void -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 	{
 		auto& info = tofu::GetTypeInfo<test::A>();
-		std::cout << info.getName() << std::endl;
-		IUTEST_ASSERT_EQ( info.getName(), std::string("test::A") );
+		std::cout << "-- test::A -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
+		IUTEST_ASSERT_EQ( info.GetNamePtr(), std::string("test::A") );
 	}
 	{
 		auto& info = tofu::GetTypeInfo<test::B>();
-		std::cout << info.getName() << std::endl;
+		std::cout << "-- test::B -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 	{
-		auto& info = tofu::GetTypeInfo<C>();
-		std::cout << info.getName() << std::endl;
+		auto& info = tofu::GetTypeInfo<const C&>();
+		std::cout << "-- const C& -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
+		std::cout << info.GetName() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 	{
 		auto& info = tofu::GetTypeInfo<int&>();
-		std::cout << info.getName() << std::endl;
+		std::cout << "-- int& -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 	{
 		auto& info = tofu::GetTypeInfo<int*>();
-		std::cout << info.getName() << std::endl;
+		std::cout << "-- int* -- " << std::endl;
+		std::cout << info.DBG_GetTypeNameSample() << std::endl;
+		std::cout << info.GetNamePtr() << std::endl;
 	}
 }
