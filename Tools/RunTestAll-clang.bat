df7a4ffe6b02ea38b208d69b5f6e454af8230bb2
YASU-NOTE\dinks <dinks223@gmail.com>
2025-03-26 00:44:01 +0900

    [tools] RunTestで対象をbat引数で指定できるように拡張

--- Tools/RunTestAll-clang.bat
+++ Tools/RunTestAll-clang.bat
@@ -2,7 +2,11 @@
 setlocal enabledelayedexpansion
 pushd %~dp0
 
-..\Build\vs2022\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=*
+if "%1" == "" (
+	..\Build\vs2022\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=*
+) else (
+	..\Build\vs2022\tofu_test\x64\Debug\tofu_test.exe --iutest_filter=%1
+)
 
 echo Done Debug-clang
 
